88bae2b581f237f49f986abf98a754367d192fdf
CCHo <royho@MacBook-Pro.local>
2017-01-28 00:44:57 -0800

    Initial Commit

---
+++ Black_Jack_CPP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Black_Jack_CPP
+//
+//  Created by CCHo on 2017/1/28.
+//  Copyright © 2017年 UCD. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
